8701fcfcbd4e4658e64a5adc1063b51d914442f1
Artem Shavriev <artem-shavriev@yandex.ru>
2024-11-06 11:39:02 +0300

    ФЗ: 10 - Добавить класс FilmController.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,86 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+    final int maxDescriptionLength = 200;
+    final Instant minReleaseDate = Instant.parse("1985-12-28");
+
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return films.values();
+    }
+
+    public String filmValidator(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            return "Название фильма не может быть пустым.";
+        }
+        if (film.getDuration().toMinutes() <= 0) {
+           return "Продолжительность фильма должна быть положительным числом.";
+        }
+        if (film.getDescription().length() > maxDescriptionLength) {
+           return"Максимальная длина описания не должна превышать 200 символов.";
+        }
+        if (film.getReleaseDate().isBefore(minReleaseDate)) {
+            return "Дата релиза должна быть не раньше 28 декабря 1895 года";
+        }
+        return "true";
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        if (!filmValidator(film).equals("true")) {
+            throw new ValidationException(filmValidator(film));
+        }
+
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+
+        return film;
+    }
+
+    private int getNextId() {
+        int currentMaxId = films.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        if (newFilm.getId() == null) {
+            throw new ValidationException("id должен быть указан.");
+        }
+        if (films.containsKey(newFilm.getId())) {
+            if (!filmValidator(newFilm).equals("true")) {
+                throw new ValidationException(filmValidator(newFilm));
+            }
+            Film oldFilm = films.get(newFilm.getId());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(newFilm.getDuration());
+            return oldFilm;
+        }
+        throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
+    }
 }
