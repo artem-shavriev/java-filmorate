14f5e9be576fcecd5375e80fa99fbfe5ded51ea9
Artem Shavriev <artem-shavriev@yandex.ru>
2024-11-06 13:17:54 +0300

    ФЗ: 10 - Добавить логирование.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +16,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -32,11 +34,16 @@ public class FilmController {
         if (film.getName() == null || film.getName().isBlank()) {
             return "Название фильма не может быть пустым.";
         }
+        for (Film f : films.values()) {
+            if (f.getName().equals(film.getName())) {
+                return "Фильм с таким названием уже есть в списке.";
+            }
+        }
         if (film.getDuration().toMinutes() <= 0) {
            return "Продолжительность фильма должна быть положительным числом.";
         }
         if (film.getDescription().length() > maxDescriptionLength) {
-           return"Максимальная длина описания не должна превышать 200 символов.";
+            return "Максимальная длина описания не должна превышать 200 символов.";
         }
         if (film.getReleaseDate().isBefore(minReleaseDate)) {
             return "Дата релиза должна быть не раньше 28 декабря 1895 года";
@@ -47,13 +54,15 @@ public class FilmController {
     @PostMapping
     public Film addFilm(@RequestBody Film film) {
         if (!filmValidator(film).equals("true")) {
+            log.error(filmValidator(film));
             throw new ValidationException(filmValidator(film));
         }
 
         film.setId(getNextId());
         films.put(film.getId(), film);
+        log.info("Добавлен новыйфильм {}", film.getName());
 
-        return film;
+    return film;
     }
 
     private int getNextId() {
@@ -68,10 +77,12 @@ public class FilmController {
     @PutMapping
     public Film updateFilm(@RequestBody Film newFilm) {
         if (newFilm.getId() == null) {
+            log.error("id должен быть указан.");
             throw new ValidationException("id должен быть указан.");
         }
         if (films.containsKey(newFilm.getId())) {
             if (!filmValidator(newFilm).equals("true")) {
+                log.error(filmValidator(newFilm));
                 throw new ValidationException(filmValidator(newFilm));
             }
             Film oldFilm = films.get(newFilm.getId());
@@ -79,8 +90,10 @@ public class FilmController {
             oldFilm.setName(newFilm.getName());
             oldFilm.setReleaseDate(newFilm.getReleaseDate());
             oldFilm.setDuration(newFilm.getDuration());
+            log.info("Фильм был обновлен.");
             return oldFilm;
         }
+        log.error("Фильм с id = {} не найден", newFilm.getId());
         throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +16,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -28,6 +30,11 @@ public class UserController {
     public String userValidator(User user) {
         if (user.getEmail() == null || user.getEmail().isBlank()) {
             return "Электронная почта должна быть указана.";
+        }
+        for (User u : users.values()) {
+            if (u.getEmail().equals(user.getEmail())) {
+                return "Электронная почта уже используется";
+            }
         }
         if (user.getEmail().indexOf("@") < 0) {
             return "Электронная почта должна должна содержать @";
@@ -53,14 +60,17 @@ public class UserController {
     @PostMapping
     public User addUser(@RequestBody User user) {
         if (!userValidator(user).equals("true")) {
+            log.error(userValidator(user));
             throw new ValidationException(userValidator(user));
         }
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
+            log.warn("Имя не передано, его заменит логин.");
         }
 
         user.setId(getNextId());
         users.put(user.getId(), user);
+        log.info("Создан новый польхователь.");
 
         return user;
     }
@@ -68,10 +78,12 @@ public class UserController {
     @PutMapping
     public User updateUser(@RequestBody User newUser) {
         if (newUser.getId() == null) {
+            log.error("Id не куказан.");
             throw new ValidationException("Id должен быть указан");
         }
         if (users.containsKey(newUser.getId())) {
             if (!userValidator(newUser).equals("true")) {
+                log.error(userValidator(newUser));
                 throw new ValidationException(userValidator(newUser));
             }
 
@@ -80,9 +92,11 @@ public class UserController {
             oldUser.setBirthday(newUser.getBirthday());
             oldUser.setLogin(newUser.getLogin());
             oldUser.setEmail(newUser.getEmail());
+            log.info("Добавлен новый пользователь.");
 
             return oldUser;
         }
+        log.error("User с id = {} не найден", newUser.getId());
         throw new NotFoundException("User с id = " + newUser.getId() + " не найден");
     }
 }
