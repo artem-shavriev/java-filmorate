15dc256606edbf84e86b0ef160edd7e01ea5759e
Artem Shavriev <artem-shavriev@yandex.ru>
2024-11-08 12:30:48 +0300

    ФЗ: 10 Удалить импорт

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 
 @SpringBootTest
 class FilmorateApplicationTests {
