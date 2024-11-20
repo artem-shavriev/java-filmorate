6b10bdcfc5cc9a11f622ad58175e12eccb0527d3
Artem Shavriev <artem-shavriev@yandex.ru>
2024-11-20 13:58:30 +0300

    ФЗ: 11 Добавить методы getFriends и commonFriends.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FilmService {
-    
+    InMemoryFilmStorage inMemoryFilmStorage;
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -8,7 +8,10 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -19,11 +22,11 @@ public class UserService {
     public User addFriend(Long userId, Long friendId) {
         HashMap<Long, User> users = (HashMap<Long, User>) inMemoryUserStorage.getUsers();
 
-        if (!users.keySet().contains(userId)) {
+        if (!users.containsKey(userId)) {
             log.error("userId не найден.");
             throw new NotFoundException("userId не найден.");
         }
-        if (!users.keySet().contains(friendId)) {
+        if (!users.containsKey(friendId)) {
             log.error("friendId не найден.");
             throw new NotFoundException("friendId не найден.");
         }
@@ -42,12 +45,12 @@ public class UserService {
     public User deleteFriend(Long userId, Long friendId) {
         HashMap<Long, User> users = (HashMap<Long, User>) inMemoryUserStorage.getUsers();
 
-        if (!users.keySet().contains(userId)) {
-            log.error("userId не найден.");
+        if (!users.containsKey(userId)) {
+            log.error("userId не существует.");
             throw new NotFoundException("userId не найден.");
         }
-        if (!users.keySet().contains(friendId)) {
-            log.error("friendId не найден.");
+        if (!users.containsKey(friendId)) {
+            log.error("friendId не существует.");
             throw new NotFoundException("friendId не найден.");
         }
 
@@ -62,6 +65,48 @@ public class UserService {
         return users.get(friendId);
     }
 
+    public List<User> getFriends(Long userId) {
+        HashMap<Long, User> users = (HashMap<Long, User>) inMemoryUserStorage.getUsers();
 
+        if (!users.containsKey(userId)) {
+            log.error("Переданный userId не существует.");
+            throw new NotFoundException("userId не найден.");
+        }
+
+        List<User> friendsList = new ArrayList<>();
+
+        if (users.get(userId).getFriendsId() == null) {
+            log.error("У пользовтеля с id: {} пустой список друзей", userId);
+            throw new NotFoundException("У пользовтеля пустой список друзей.");
+        }
+
+        Set<Long> friendsId = users.get(userId).getFriendsId();
+
+        for(Long id: friendsId) {
+            friendsList.add(users.get(id));
+        }
+        log.info("Список друзей пользователя с id {} сформирован.", userId );
+        return friendsList;
+    }
+
+    public List<User> commonFriends(Long userId, Long otherId) {
+        HashMap<Long, User> users = (HashMap<Long, User>) inMemoryUserStorage.getUsers();
+
+        if (!users.containsKey(userId)) {
+            log.error("Данного userId не существует.");
+            throw new NotFoundException("userId не найден.");
+        }
+        if (!users.containsKey(otherId)) {
+            log.error("Данного otherId не существует.");
+            throw new NotFoundException("friendId не найден.");
+        }
+
+        List<User> userfriendsList = getFriends(userId);
+        List<User> otherUserfriendsList = getFriends(otherId);
+        userfriendsList.retainAll(otherUserfriendsList);
+
+        log.info("Список общих друзей пользователей с id: {} и {} сформирован.", userId, otherId );
+        return userfriendsList;
+    }
 }
 
