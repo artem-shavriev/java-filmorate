f2eb82f888334ffe03ca1bbde93d0c6279a17986
Artem Shavriev <artem-shavriev@yandex.ru>
2024-11-20 14:47:54 +0300

    ФЗ: 11 Добавить метод likeThisFilm.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 public class Film {
@@ -20,4 +21,5 @@ public class Film {
     @NotNull
     @Positive (message = "Продолжительность должна быть положительным числом")
     private Integer duration;
+    private Set<Long> likesFromUsers;
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,11 +3,43 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.DuplicateException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+
+import java.util.HashMap;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FilmService {
     InMemoryFilmStorage inMemoryFilmStorage;
+    InMemoryUserStorage inMemoryUserStorage;
+
+    public Film likeThisFilm(Long filmId, Long userId) {
+        HashMap<Long, Film> films = (HashMap<Long, Film>) inMemoryFilmStorage.getFilms();
+        HashMap<Long, User> users = (HashMap<Long, User>) inMemoryUserStorage.getUsers();
+
+        if (!films.containsKey(filmId)) {
+            log.error("Фильм с id: {} не существует.", filmId);
+            throw new NotFoundException("Фильм с данным id не существует.");
+        }
+
+        if (!users.containsKey(userId)) {
+            log.error("Пользовтель с id: {} не сущетвует.", userId);
+            throw new NotFoundException("Пользовтель с данным id не сущетвует.");
+        }
+
+        if (films.get(filmId).getLikesFromUsers().contains(userId)) {
+            log.error("Пользовтель с id: {} уже лайкал фильм с id: {}.", userId, filmId);
+            throw new DuplicateException("Данный пользовтаель уж елайкал этот фильм.");
+        }
+        films.get(filmId).getLikesFromUsers().add(userId);
+        log.info("Пользовтель с id {} лайкнул фильм с id {}.", userId, filmId);
+
+        return films.get(filmId);
+    }
 }
