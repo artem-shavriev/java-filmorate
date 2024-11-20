0cb0a1034b166b283b7cba58cbfe3944fd18802b
Artem Shavriev <artem-shavriev@yandex.ru>
2024-11-20 14:52:34 +0300

    ФЗ: 11 Добавить метод deleteLikeFromFilm.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -35,11 +35,36 @@ public class FilmService {
 
         if (films.get(filmId).getLikesFromUsers().contains(userId)) {
             log.error("Пользовтель с id: {} уже лайкал фильм с id: {}.", userId, filmId);
-            throw new DuplicateException("Данный пользовтаель уж елайкал этот фильм.");
+            throw new DuplicateException("Данный пользовтаель уже лайкал этот фильм.");
         }
         films.get(filmId).getLikesFromUsers().add(userId);
         log.info("Пользовтель с id {} лайкнул фильм с id {}.", userId, filmId);
 
         return films.get(filmId);
     }
+
+    public Film deleteLikeFromFilm(Long filmId, Long userId) {
+        HashMap<Long, Film> films = (HashMap<Long, Film>) inMemoryFilmStorage.getFilms();
+        HashMap<Long, User> users = (HashMap<Long, User>) inMemoryUserStorage.getUsers();
+
+        if (!films.containsKey(filmId)) {
+            log.error("Фильм с id: {} не найден.", filmId);
+            throw new NotFoundException("Фильм с данным id не существует.");
+        }
+
+        if (!users.containsKey(userId)) {
+            log.error("Пользовтель с id: {} не найден.", userId);
+            throw new NotFoundException("Пользовтель с данным id не сущетвует.");
+        }
+
+        if (!films.get(filmId).getLikesFromUsers().contains(userId)) {
+            log.error("Пользовтель с id: {} еще не лайкал фильм с id: {}.", userId, filmId);
+            throw new DuplicateException("Данный пользовтаель еще не лайкал этот фильм.");
+        }
+        films.get(filmId).getLikesFromUsers().remove(userId);
+        log.info("Лайк пользовтеля с id {} фильму с id {} был удален.", userId, filmId);
+
+        return films.get(filmId);
+    }
+
 }
